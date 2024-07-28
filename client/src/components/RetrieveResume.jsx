d0c51665807012e93fb94ce16e5eb7976864370a
Akhira Nandan <akhiranandanp89548@gmail.com>
2024-07-28 19:13:25 +0530

    making reusable component

--- client/src/components/RetrieveResume.jsx
+++ client/src/components/RetrieveResume.jsx
@@ -1,4 +1,7 @@
-const RetrieveResume = ({
+// RetrieveResume.js
+import React from "react";
+
+function RetrieveResume({
   resumeId,
   setResumeId,
   searchName,
@@ -7,40 +10,44 @@ const RetrieveResume = ({
   retrievedResumesByName,
   handleSearchById,
   handleSearchByName,
-}) => {
-    return(
+}) {
+  const inputFields = [
+    {
+      label: "Resume ID:",
+      value: resumeId,
+      onChange: (e) => setResumeId(e.target.value),
+      onSubmit: handleSearchById,
+      submit: "Retrieve Resume Details",
+    },
+    {
+      label: "Resume Name:",
+      value: searchName,
+      onChange: (e) => setSearchName(e.target.value),
+      onSubmit: handleSearchByName,
+      submit: "Retrieve Resume Details",
+    },
+  ];
+
+  return (
     <div>
       <h1>Retrieve Resume</h1>
 
       <div className="search-resume">
-        <div className="search-with-id">
-          <form onSubmit={handleSearchById}>
-            <div>
-              <label>Resume ID:</label>
-              <input
-                type="text"
-                value={resumeId}
-                onChange={(e) => setResumeId(e.target.value)}
-              />
-            </div>
-            <button type="submit">Retrieve Resume Details</button>
-          </form>
-        </div>
-
-        <div className="search-with-name">
-          <form onSubmit={handleSearchByName}>
-            <div>
-              <label>Resume Name:</label>
-              <input
-                type="text"
-                value={searchName}
-                onChange={(e) => setSearchName(e.target.value)}
-                placeholder="First Name Last Name"
-              />
-            </div>
-            <button type="submit">Retrieve Resume Details</button>
-          </form>
-        </div>
+        {inputFields.map((item, index) => (
+          <div key={index}>
+            <form onSubmit={item.onSubmit}>
+              <div>
+                <label>{item.label}</label>
+                <input
+                  type="text"
+                  value={item.value}
+                  onChange={item.onChange}
+                />
+              </div>
+              <button type="submit">{item.submit}</button>
+            </form>
+          </div>
+        ))}
       </div>
 
       {retrievedResumeById && (
@@ -66,7 +73,7 @@ const RetrieveResume = ({
               <p>Job Title: {resume.job_title}</p>
               <p>Job Description: {resume.job_description}</p>
               <p>Job Company: {resume.job_company}</p>
-              <p>----------------------------------------------------------</p>
+              <p>---------------------------------------------------</p>
             </div>
           ))}
         </div>
